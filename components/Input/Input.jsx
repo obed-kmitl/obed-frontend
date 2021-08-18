fd1ffc9a0b99ad2528b830f999cc790eaeec9020
Nattarat Serirat <fangy2543@hotmail.com>
2021-08-18 16:59:26 +0700

    create Input component

---
+++ components/Input/Input.jsx
@@ -0,0 +1,51 @@
+import { Input, Select } from 'antd'
+import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
+import styles from './Input.module.scss';
+
+const { Option } = Select
+const { Search } = Input;
+export default function MyInput({ placeholder, width, type, addonBefore, password , search ,onSearch }) {
+
+    if (password) {
+        return (
+            <div className={styles.input}>
+                <Input.Password
+                    style={{ width: width ? width : "240px" }}
+                    placeholder={placeholder}
+                    iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+                />
+            </div>
+        )
+    }
+    if (search) {
+        return (
+            <div className={styles.input}>
+                <Search
+                    style={{ width: width ? width : "240px" }}
+                    placeholder={placeholder?placeholder:"Please Input Search"}
+                    onSearch={onSearch}
+                />
+            </div>
+        )
+
+    }
+    return (
+        <div className={styles.input}>
+            <Input
+                style={{ width: width ? width : "240px" }}
+                placeholder={placeholder}
+                type={type}
+                addonBefore={addonBefore?
+                    <Select defaultValue={addonBefore[0]} className="select-after">
+                        {addonBefore.map((item,index) =>
+                            <Option key={index} value={item}> {item}</Option>
+                        )}
+                    </Select>
+                    :null
+                }
+             
+            ></Input>
+        </div>
+    )
+
+}

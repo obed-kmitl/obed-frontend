d181a92c4f7851d71681166c63d4b226eb8af221
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2021-08-18 15:14:54 +0700

    fix undefine classname

--- components/Typography/Header.jsx
+++ components/Typography/Header.jsx
@@ -3,7 +3,7 @@ import propTypes from 'prop-types';
 import styles from './Typography.module.scss';
 
 function Header({ level, children, className }) {
-  return createElement(`h${level}`, { className: `${styles.header} ${className}`, level }, children);
+  return createElement(`h${level}`, { className: `${styles.header} ${className || ''}`, level }, children);
 }
 
 Header.propTypes = {

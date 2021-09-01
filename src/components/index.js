cd0a334b3a0d3d3d73b7dd3644ce9eef1567dbea
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2021-09-01 17:35:45 +0700

    create CourseTable as editable table

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 export { Button } from './Button/Button';
 export { Header } from './Typography/Header';
 export { Body } from './Typography/Body';
-export { Tabs, TabPane } from './Tabs/MyTabs';
+export { Tabs, TabPane } from './Tabs/Tabs';
 export { Input, TextArea } from './Input/Input';
 export { Select, Option } from './Select/Select';
 export { Collapse , Panel } from './Collapse/Collapse';
 export { TableCard } from './TableCard/TableCard';
+export { Table } from './Table/Table';
+export { CourseTable } from './CourseTable/CourseTable';

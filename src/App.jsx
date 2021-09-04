f953e2e77ac364cbb5013769a3e64a181c9a908b
thanapat722 <thanapat255@gmail.com>
2021-09-04 21:09:26 +0700

    add global.module.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
-import { BrowserRouter as Router, Switch, Route as PublicRoute } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route as PublicRoute,
+} from "react-router-dom";
 import { Curriculum, Home, Sandbox, Profile, Teacher } from "./pages";
-import { Layout } from "./components/Layout/Layout"
+import { Layout } from "./components/Layout/Layout";
+import "./styles/global.module.scss";
 
 function App() {
   return (

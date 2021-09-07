3eb30ec7d487e7deea8ae57ddb23b17b6201bf45
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2021-09-07 23:11:26 +0700

    Teacher page finish

--- src/pages/Teacher/Teacher.jsx
+++ src/pages/Teacher/Teacher.jsx
@@ -1,13 +1,19 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import styles from "./Teacher.module.scss";
 import { Helmet } from "react-helmet";
-import { Header, Button, Input, Body } from "../../components";
-import { Divider, Table, Tag, Menu, Dropdown, Modal, Form, Space } from "antd";
+import { Header, Button, Input, Option } from "../../components";
 import {
-  MoreOutlined,
-  DeleteOutlined,
-  MailOutlined,
-  EditOutlined,
-} from "@ant-design/icons";
+  Divider,
+  Table,
+  Modal,
+  Form,
+  Select,
+  Tooltip,
+  Popconfirm,
+  notification,
+  Space,
+} from "antd";
+import { DeleteOutlined, MailOutlined, EditOutlined } from "@ant-design/icons";
 import { useState } from "react";
 
 export const Teacher = () => {
@@ -15,6 +21,7 @@ export const Teacher = () => {
   const data = [
     {
       id: 1,
+      prefix: "ผศ.ดร.",
       firstname: "สมชาย",
       lastname: "ใจดี",
       username: "somchai1234",
@@ -23,6 +30,7 @@ export const Teacher = () => {
     },
     {
       id: 2,
+      prefix: "รศ.ดร.",
       firstname: "สมหญิง",
       lastname: "จริงใจ",
       username: "somying1",
@@ -31,6 +39,7 @@ export const Teacher = () => {
     },
     {
       id: 3,
+      prefix: "อ.",
       firstname: "สมปอง",
       lastname: "สุขสบาย",
       username: "sompong1988",
@@ -39,6 +48,7 @@ export const Teacher = () => {
     },
     {
       id: 4,
+      prefix: "ดร.",
       firstname: "สมปราชญ์",
       lastname: "สดใส",
       username: "somprach38",
@@ -47,6 +57,7 @@ export const Teacher = () => {
     },
     {
       id: 5,
+      prefix: "ผศ.",
       firstname: "สมหมาย",
       lastname: "สายไทย",
       username: "sommai55",
@@ -55,6 +66,7 @@ export const Teacher = () => {
     },
     {
       id: 6,
+      prefix: "ผศ.ดร.",
       firstname: "สมหมาย",
       lastname: "รักไทย",
       username: "sommai1999",
@@ -63,6 +75,7 @@ export const Teacher = () => {
     },
     {
       id: 7,
+      prefix: "รศ.",
       firstname: "สมศักดิ์",
       lastname: "ใฝ่รู้",
       username: "somsak74",
@@ -71,14 +84,16 @@ export const Teacher = () => {
     },
     {
       id: 8,
-      firstname: "สมศรี",
+      prefix: "อ.",
+      firstname: "กมลชนก",
       lastname: "ศรีไทย",
-      username: "somsri6854",
-      email: "somsri.sr@kmitl.ac.th",
+      username: "kamol123",
+      email: "kamolchanok.sr@kmitl.ac.th",
       status: 1,
     },
     {
       id: 9,
+      prefix: "ดร.",
       firstname: "สมพงศ์",
       lastname: "ชัยชนะ",
       username: "somphong",
@@ -87,6 +102,7 @@ export const Teacher = () => {
     },
     {
       id: 10,
+      prefix: "รศ.",
       firstname: "สมสง่า",
       lastname: "ราศี",
       username: "somsanga34",
@@ -95,28 +111,42 @@ export const Teacher = () => {
     },
     {
       id: 11,
-      firstname: "สมเกิน",
-      lastname: "อีกหน้า",
-      username: "somkoen96",
-      email: "somkoen.ei@kmitl.ac.th",
+      prefix: "อ.",
+      firstname: "ธนวัฒน์",
+      lastname: "สมมุติ",
+      username: "thanawat88",
+      email: "thanawat.so@kmitl.ac.th",
       status: 1,
     },
   ];
   const [filterList, setFilterList] = useState(data);
+  const [page, setPage] = useState(1);
   const [visible, setVisible] = useState(false);
   const [confirmLoading, setConfirmLoading] = useState(false);
   const [form] = Form.useForm();
-  const titleSelector = [
-    "ศ.ดร.",
-    "ศ.",
-    "รศ.ดร.",
-    "รศ.",
-    "ผศ.ดร.",
-    "ผศ.",
-    "ดร.",
-  ];
+  const [selectedData, setSelectedData] = useState(null);
+  const [edit, setEdit] = useState(false);
+  const selectBefore = (
+    <Form.Item name="prefix" noStyle>
+      <Select
+        className="select-before"
+        style={{ width: "85px" }}
+        placeholder="Prefix"
+      >
+        <Option value="ศ.ดร.">ศ.ดร.</Option>
+        <Option value="ศ.">ศ.</Option>
+        <Option value="รศ.ดร.">รศ.ดร.</Option>
+        <Option value="รศ.">รศ.</Option>
+        <Option value="ผศ.ดร.">ผศ.ดร.</Option>
+        <Option value="ผศ.">ผศ.</Option>
+        <Option value="ดร.">ดร.</Option>
+        <Option value="อ.">อ.</Option>
+      </Select>
+    </Form.Item>
+  );
 
-  const showModal = () => {
+  const showModal = (record = {}) => {
+    setSelectedData(record);
     setVisible(true);
   };
 
@@ -139,15 +169,23 @@ export const Teacher = () => {
   function handleSubmit(values) {
     console.log("Recieved values of form: ", values);
     setConfirmLoading(true);
+    openNotificationWithIcon(
+      "success",
+      "Teacher added",
+      "Please check user " + values.email + " inbox to change password."
+    );
     setTimeout(() => {
       setVisible(false);
       setConfirmLoading(false);
+      setFilterList([...data, values]);
     }, 2000);
   }
 
   function handleCancel() {
     setVisible(false);
     form.resetFields();
+    setSelectedData(null);
+    setEdit(false);
   }
 
   function onFinish(values) {
@@ -158,20 +196,53 @@ export const Teacher = () => {
     console.log("Failed:", errorInfo);
   }
 
-  const menu = (
-    <Menu>
-      <Menu.Item icon={<EditOutlined />} key="1">
-        <a href="#">Edit</a>
-      </Menu.Item>
-      <Menu.Item icon={<MailOutlined />} key="2">
-        <a href="#">Resend Password</a>
-      </Menu.Item>
-      <Menu.Divider />
-      <Menu.Item danger icon={<DeleteOutlined />} key="3">
-        Disable Account
-      </Menu.Item>
-    </Menu>
-  );
+  function openNotificationWithIcon(type, message, desc) {
+    notification[type]({
+      message: message,
+      description: desc,
+      duration: 5,
+    });
+  }
+
+  function resendPassword(record) {
+    let success = true;
+    if (success)
+      openNotificationWithIcon(
+        "success",
+        "Request sent to " + record.email,
+        "Please check user email inbox to change password."
+      );
+    else
+      openNotificationWithIcon(
+        "error",
+        "Request Error",
+        "There's error sending request, Please try again."
+      );
+  }
+
+  function deleteAccount(record) {
+    let temp = filterList.filter((e) => e.id !== record.id);
+    setFilterList(temp);
+    console.log(temp);
+  }
+
+  // const menu = (record) => (
+  //   <Menu>
+  //     <Menu.Item icon={<MailOutlined />} key="2">
+  //       <a href="#" onClick={() => resendPassword()}>
+  //         Change Password
+  //       </a>
+  //     </Menu.Item>
+  //     <Menu.Item danger icon={<DeleteOutlined />} key="3">
+  //       <Popconfirm
+  //         title="Sure to delete this account?"
+  //         onConfirm={() => deleteAccount(record)}
+  //       >
+  //         <a href="#">Delete Account</a>
+  //       </Popconfirm>
+  //     </Menu.Item>
+  //   </Menu>
+  // );
   return (
     <div className={styles.container}>
       <Helmet>
@@ -180,42 +251,138 @@ export const Teacher = () => {
       <div className={styles.head}>
         <Header level={1}>Teacher</Header>
         <div>
-          <Input search placeholder="Search" onSearch={search} />
+          <Input search placeholder="Search" onSearch={search} allowClear />
           <Button onClick={showModal}>Add</Button>
         </div>
       </div>
       <Divider />
-      <Table dataSource={filterList} rowKey="id">
-        <Column title="ID" dataIndex="id" key="id" />
-        <Column title="First Name" dataIndex="firstname" key="firstname" />
-        <Column title="Last Name" dataIndex="lastname" key="lastname" />
+      <Table
+        dataSource={filterList}
+        rowKey="id"
+        pagination={{
+          onChange(current) {
+            setPage(current);
+          },
+        }}
+        bordered
+      >
+        <Column
+          title="No."
+          dataIndex="index"
+          key="index"
+          width="60px"
+          render={(value, item, index) => (page - 1) * 10 + index + 1}
+        />
+        <Column
+          title="Prefix"
+          dataIndex="prefix"
+          key="prefix"
+          width="100px"
+          filters={[
+            {
+              text: "อ.",
+              value: "อ.",
+            },
+            {
+              text: "ศ.ดร.",
+              value: "ศ.ดร.",
+            },
+            {
+              text: "ศ.",
+              value: "ศ.",
+            },
+            {
+              text: "รศ.ดร.",
+              value: "รศ.ดร.",
+            },
+            {
+              text: "รศ.",
+              value: "รศ.",
+            },
+            {
+              text: "ผศ.ดร.",
+              value: "ผศ.ดร.",
+            },
+            {
+              text: "ผศ.",
+              value: "ผศ.",
+            },
+            {
+              text: "ดร.",
+              value: "ดร.",
+            },
+          ]}
+          onFilter={(value, record) => record.prefix.indexOf(value) === 0}
+        />
+        <Column
+          title="Name"
+          dataIndex="firstname"
+          key="firstname"
+          sorter={(a, b) => a.firstname.localeCompare(b.firstname)}
+          render={(text, record) => text + " " + record.lastname}
+          defaultSortOrder="ascend"
+          sortDirections={["ascend", "descend", "ascend"]}
+        />
         <Column title="Username" dataIndex="username" key="username" />
         <Column title="Email" dataIndex="email" key="email" />
         <Column
-          title="Status"
-          dataIndex="status"
-          key="status"
-          render={(tag) => (
-            <>
-              <Tag
-                color={tag === 1 ? "green" : tag === 0 ? "gold" : "red"}
-                key={tag}
-              >
-                {tag === 1
-                  ? "Verified"
-                  : tag === 0
-                  ? "Not Verified"
-                  : "Disabled"}
-              </Tag>
-            </>
-          )}
-        />
-        <Column
           title="Action"
           key="action"
-          width="15px"
-          render={(ele) => (
-            <Dropdown overlay={menu}>
+          width="30px"
+          render={(ele, record) => (
+            <Space size="large">
+              <Tooltip title="Edit">
+                <a
+                  href="#"
+                  onClick={() => {
+                    setEdit(true);
+                    form.setFieldsValue(record);
+                    showModal();
+                  }}
+                  style={{
+                    fontSize: "20px",
+                    color: "#009FC7",
+                  }}
+                >
+                  <EditOutlined />
+                </a>
+              </Tooltip>
+              <Tooltip title="Change Password">
+                <a
+                  href="#"
+                  style={{
+                    fontSize: "20px",
+                    color: "#009FC7",
+                  }}
+                  onClick={() => resendPassword(record)}
+                >
+                  <MailOutlined />
+                </a>
+              </Tooltip>
+              <Tooltip title="Delete">
+                <Popconfirm
+                  title="Sure to delete this account?"
+                  onConfirm={() => deleteAccount(record)}
+                >
+                  <a
+                    href="#"
+                    style={{
+                      fontSize: "20px",
+                      color: "#C73535",
+                    }}
+                  >
+                    <DeleteOutlined />
+                  </a>
+                </Popconfirm>
+              </Tooltip>
+            </Space>
+          )}
+        />
+        {/* <Column
+          key="action"
+          width="30px"
+          render={(ele, record) => (
+            <Dropdown overlay={() => menu(record)}>
               <a
                 className="ant-dropdown-link"
                 onClick={(e) => e.preventDefault()}
@@ -225,10 +392,10 @@ export const Teacher = () => {
               </a>
             </Dropdown>
           )}
-        />
+        /> */}
       </Table>
       <Modal
-        title="Add Teacher"
+        title={edit ? "Edit Teacher" : "Add Teacher"}
         visible={visible}
         okText="Add"
         onOk={() => {
@@ -244,7 +411,7 @@ export const Teacher = () => {
         }}
         confirmLoading={confirmLoading}
         onCancel={handleCancel}
-        // okButtonProps={{ htmlType: "submit" }}
+        okButtonProps={{ htmlType: "submit" }}
         maskClosable={false}
         width="700px"
         // footer={
@@ -270,7 +437,7 @@ export const Teacher = () => {
           form={form}
           name="teacher"
           layout="vertical"
-          initialValues={{ remember: false }}
+          initialValues={selectedData}
           onFinish={onFinish}
           onFinishFailed={onFinishFailed}
           autoComplete="off"
@@ -281,7 +448,7 @@ export const Teacher = () => {
             name="firstname"
             rules={[{ required: true, message: "Please input firstname!" }]}
           >
-            <Input placeholder="Firstname" addonBefore={titleSelector} />
+            <Input placeholder="Firstname" addonBefore={selectBefore} />
           </Form.Item>
           <Form.Item
             label="Lastname"

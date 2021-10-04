8c00bc0063960aacd26a42dbbf8266edc621fbb3
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2021-10-04 22:41:04 +0700

    useUser hook to get current user info

---
+++ src/hooks/useUser.js
@@ -0,0 +1,36 @@
+import { useContext, useState } from "react";
+import { useHistory } from "react-router";
+
+import httpClient from "../utils/httpClient";
+import UserContext from "../contexts/UserContext";
+
+const useUser = () => {
+  const history = useHistory();
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { setUser } = useContext(UserContext);
+
+  function getProfile() {
+    return httpClient.get("/user/getProfile").then(
+      (response) => {
+        localStorage.setItem("user", JSON.stringify(response.data.data));
+        console.log("useUser", response.data.data);
+        setUser(response.data.data);
+        return response.data.data;
+      },
+      (error) => {
+        const resMessage =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+        setMessage(resMessage);
+      }
+    );
+  }
+
+  return { getProfile, message, loading };
+};
+
+export default useUser;

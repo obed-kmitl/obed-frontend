daee36453fc3c6d97d591bb48848196eb0deec94
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2021-10-21 22:37:25 +0700

    fix error handler on no connection

--- src/hooks/useAuthen.js
+++ src/hooks/useAuthen.js
@@ -31,24 +31,32 @@ const useAuthen = () => {
         },
         (error) => {
           let resMessage = "";
-          switch (error.response.data.error.code) {
-            case "UNAUTHORIZED":
-              resMessage = "Wrong username or password.";
-              break;
-            case "INTERNAL_SERVER_ERROR":
-              resMessage = "Something went wrong, Please check and try again.";
-              break;
-            default:
-              resMessage =
-                (error.response &&
-                  error.response.data &&
-                  error.response.data.message) ||
-                error.message ||
-                error.toString();
-              break;
+          if (error.response && error.response.data) {
+            switch (error.response.data.error.code) {
+              case "UNAUTHORIZED":
+                resMessage = "Wrong username or password.";
+                break;
+              case "INTERNAL_SERVER_ERROR":
+                resMessage =
+                  "Something went wrong, Please check and try again.";
+                break;
+              default:
+                resMessage =
+                  (error.response &&
+                    error.response.data &&
+                    error.response.data.message) ||
+                  error.message ||
+                  error.toString();
+                break;
+            }
+            setMessage(resMessage);
+            setLoading(false);
+          } else {
+            setMessage(
+              "Cannot connect to server, Please check connection and try again."
+            );
+            setLoading(false);
           }
-          setMessage(resMessage);
-          setLoading(false);
         }
       );
   }

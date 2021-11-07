5212aa1e0bc06fff6a806ea7f13116fb5c6917ac
Nattarat Serirat <fangy2543@hotmail.com>
2021-11-07 14:01:23 +0700

    add filter

--- src/components/ActivityList/ActivityList.jsx
+++ src/components/ActivityList/ActivityList.jsx
@@ -1,5 +1,5 @@
 import styles from './ActivityList.module.scss'
-import { Collapse, Panel, Header } from '..'
+import { Collapse, Panel, Header, Select, Option } from '..'
 
 const catagory = [
     {
@@ -29,19 +29,34 @@ const catagory = [
     },
 ]
 
+
 export const ActivityList = () => {
     return (
         <div className={styles.activityList}>
+            <div className={styles.filter}>
+                Group:
+                <Select width={150} defaultValue="ALL">
+                    <Option value="ALL">All</Option>
+                    <Option value="INDIVIDUAL">Individual</Option>
+                    <Option value="GROUP">Group</Option>
+                </Select>
+                Type:
+                <Select width={150} defaultValue="ALL">
+                    <Option value="ALL">All</Option>
+                    <Option value="SINGLE">Single</Option>
+                    <Option value="MULTIPLE">Multiple</Option>
+                </Select>
+            </div>
             <Collapse
                 ghost
-                activeKey={catagory.map((cat)=>cat.id)}
+                defaultActiveKey={catagory.map((cat) => cat.id)}
             >
                 {catagory.map((cat) =>
                     <Panel
                         header={
                             <div style={{ display: "flex", justifyContent: "space-between" }}>
                                 <Header level={3}>{cat.catagory}</Header>
-                                <Header level={4}>{cat.weight}%</Header>
+                                <Header style={{ color: "#F7941D" }} level={4}>{cat.weight}%</Header>
                             </div>
                         }
                         key={cat.id}

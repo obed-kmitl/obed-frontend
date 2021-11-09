767108c76b5f242c503e51022ab31d344b747750
Nattarat Serirat <fangy2543@hotmail.com>
2021-11-09 16:21:20 +0700

    create mock all semester

--- src/pages/Planning/hooks/useWeighting.js
+++ src/pages/Planning/hooks/useWeighting.js
@@ -2,33 +2,33 @@ import { useState } from 'react'
 import { message,Form } from 'antd'
 
 
-const weighting = [
-    {
-        id: 1,
-        catagory: "Homework",
-        weight: 20
-    },
-    {
-        id: 2,
-        catagory: "Assignment",
-        weight: 20
-    },
-    {
-        id: 3,
-        catagory: "Examination",
-        weight: 50
-    },
-    {
-        id: 4,
-        catagory: "Attendance",
-        weight: 5
-    },
-    {
-        id: 5,
-        catagory: "Quiz",
-        weight: 5
-    },
-]
+// const weighting = [
+//     {
+//         id: 1,
+//         catagory: "Homework",
+//         weight: 20
+//     },
+//     {
+//         id: 2,
+//         catagory: "Assignment",
+//         weight: 20
+//     },
+//     {
+//         id: 3,
+//         catagory: "Examination",
+//         weight: 50
+//     },
+//     {
+//         id: 4,
+//         catagory: "Attendance",
+//         weight: 5
+//     },
+//     {
+//         id: 5,
+//         catagory: "Quiz",
+//         weight: 5
+//     },
+// ]
 
 export const useWeighting = () => {
     const [weightingList, setWeightingList] = useState([])

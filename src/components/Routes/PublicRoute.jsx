76c88281687015d1b1b37fb917c4178a9b370a43
noppanat.pa <noppanat.pa@easysunday.com>
2021-12-31 00:59:24 +0700

    integrate with backend

--- src/components/Routes/PublicRoute.jsx
+++ src/components/Routes/PublicRoute.jsx
@@ -1,20 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect } from "react";
-import { Route, useHistory } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 const PublicRoute = ({ children, path, ...props }) => {
-  const history = useHistory();
-
-  function checkSession() {
-    if (localStorage.getItem("atk")) {
-      return history.push("/");
-    }
-  }
-
-  useEffect(() => {
-    checkSession();
-  }, []);
-
   return (
     <Route path={path} {...props}>
       {children}

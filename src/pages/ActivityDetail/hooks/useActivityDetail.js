051d966c8308e3154ee4f50194597c3f47e625d1
Nattarat Serirat <fangy2543@hotmail.com>
2022-01-03 17:27:45 +0700

    add title and score edit /group tab disable check

--- src/pages/ActivityDetail/hooks/useActivityDetail.js
+++ src/pages/ActivityDetail/hooks/useActivityDetail.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 const Activity = {
     
@@ -6,8 +6,7 @@ const Activity = {
         title: "Activity-1: Number base conversion - base 2 to base 10",
         description: "แปลงเลขต่อไปนี้จากเลขฐาน 2 ไปยัง เลขฐาน 10 แบบไม่คิดเครื่องหมาย Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam accusantium a non. Doloribus libero minus optio fugit perferendis natus voluptate, quaerat ut nemo laboriosam delectus ea nisi, saepe fuga enim! Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam accusantium a non. Doloribus libero minus optio fugit perferendis natus voluptate, quaerat ut nemo laboriosam delectus ea nisi, saepe fuga enim!"  ,
         catagory_id: 1,
-        type: "Individual",
-        sub_activity: "Single",
+        type: "Group",
         total_score: 100,
     
 }
@@ -44,5 +43,7 @@ const catagories = [
 export const useActivityDetail = () => {
     const [activity,setActivity] = useState(Activity)
     const [catagory,setCatagory] = useState(catagories)
-    return {activity,catagory}
+    
+
+    return {activity,setActivity,catagory}
 }

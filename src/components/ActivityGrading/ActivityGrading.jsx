213121560739844189349bb2cc127a6d25ce6031
Nattarat Serirat <fangy2543@hotmail.com>
2022-01-12 16:25:03 +0700

    fix table rerender

--- src/components/ActivityGrading/ActivityGrading.jsx
+++ src/components/ActivityGrading/ActivityGrading.jsx
@@ -1,389 +1,125 @@
-import { GradingTable } from "./GradingTable"
-import { useState,useEffect } from "react";
+//import { GradingTable } from "./GradingTable"
+import { useState, useEffect } from "react";
+import { useActivityGrading } from "./hooks/useActivityGrading";
+import { Tooltip, Tabs } from "antd";
+import { Button, Collapse, Panel, Header, Body } from "..";
+import styles from './ActivityGrading.module.scss'
 
-const students_score = [
-    {
-        id: 61010001,
-        prefix: "นางสาว",
-        firstname: "กมลชนก",
-        lastname: "ศรีไทย",
-        email: "61010001@kmitl.ac.th",
-        score: [
-            {
-                sub_activity_id:1,
-                max_score:2,
-                obtained_score:1.5
+export const ActivityGrading = ({ activity }) => {
+    const { students, setStudents, subActivity, rubrics, handleSelectRubric } = useActivityGrading()
 
-            },
-            {
-                sub_activity_id:2,
-                max_score:2,
-                obtained_score:2
+    const Rubric = ({studentId, sub_activity_id}) => {
+        const [defRubric, setDefRubric] = useState();
 
-            },
-            {
-                sub_activity_id:3,
-                max_score:2,
-                obtained_score:0.5
+        useEffect(() => {
+            let arr = rubrics.sort((a, b) => a.point - b.point);
+            setDefRubric(arr);
+        }, []);
 
-            },
-            {
-                sub_activity_id:4,
-                max_score:2,
-                obtained_score:2
+        return (
+            <div className={styles.rubricWrap}>
+                {defRubric?.map((ele, i) => (
+                    <div className={styles.rubric} key={"def" + i} onClick={() => handleSelectRubric(ele.point,studentId, sub_activity_id)}>
+                        <Header level={4} className={styles.level}>
+                            Level {i + 1}
+                        </Header>
+                        <Body level={3} className={styles.desc}>
+                            {ele.desc}
+                        </Body>
+                        <Header level={5} className={styles.point}>{ele.point} Points</Header>
+                    </div>
+                ))}
+            </div>
+        )
+    }
 
-            }
-        ],
-        score_status:"Finished"
-    },
-    {
-        id: 61010002,
-        prefix: "นาย",
-        firstname: "ธนวัฒน์",
-        lastname: "สมมุติ",
-        email: "61010002@kmitl.ac.th",
-        score: [
-            {
-                sub_activity_id:1,
-                max_score:2,
-                obtained_score:1.5
-
-            },
-            {
-                sub_activity_id:2,
-                max_score:2,
-                obtained_score:2
-
-            },
-            {
-                sub_activity_id:3,
-                max_score:2,
-                obtained_score:2
-
-            },
-            {
-                sub_activity_id:4,
-                max_score:2,
-                obtained_score:2
-
-            }
-        ],
-        score_status:"Finished"
-    },
-    {
-        id: 61010003,
-        prefix: "นาย",
-        firstname: "สมปอง",
-        lastname: "สุขสบาย",
-        email: "61010003@kmitl.ac.th",
-        score: [
-            {
-                sub_activity_id:1,
-                max_score:2,
-                obtained_score:1.5
-
-            },
-            {
-                sub_activity_id:2,
-                max_score:2,
-                obtained_score:2
-
-            },
-            {
-                sub_activity_id:3,
-                max_score:2,
-                obtained_score:0.5
-
-            },
-            {
-                sub_activity_id:4,
-                max_score:2,
-                obtained_score:0
-
-            }
-        ],
-        score_status:"Finished"
-    },
-    {
-        id: 61010004,
-        prefix: "นาย",
-        firstname: "สมปราชญ์",
-        lastname: "สดใส",
-        email: "61010004@kmitl.ac.th",
-        score: [
-            {
-                sub_activity_id:1,
-                max_score:2,
-                obtained_score:1.5
-
-            },
-            {
-                sub_activity_id:2,
-                max_score:2,
-                obtained_score:2
-
-            },
-            {
-                sub_activity_id:3,
-                max_score:2,
-                obtained_score:null
-
-            },
-            {
-                sub_activity_id:4,
-                max_score:2,
-                obtained_score:2
-
-            }
-        ],
-        score_status:"Not Finished"
-    },
-    {
-        id: 61010005,
-        prefix: "นาย",
-        firstname: "สมหมาย",
-        lastname: "สายไทย",
-        email: "61010005@kmitl.ac.th",
-        score: [
-            {
-                sub_activity_id:1,
-                max_score:2,
-                obtained_score:1
-
-            },
-            {
-                sub_activity_id:2,
-                max_score:2,
-                obtained_score:2
-
-            },
-            {
-                sub_activity_id:3,
-                max_score:2,
-                obtained_score:0.5
-
-            },
-            {
-                sub_activity_id:4,
-                max_score:2,
-                obtained_score:null
-
-            }
-        ],
-        score_status:"Not Finished"
-    },
-    {
-        id: 61010006,
-        prefix: "นาย",
-        firstname: "สมหมาย",
-        lastname: "รักไทย",
-        email: "61010006@kmitl.ac.th",
-        score: [
-            {
-                sub_activity_id:1,
-                max_score:2,
-                obtained_score:1.5
-
-            },
-            {
-                sub_activity_id:2,
-                max_score:2,
-                obtained_score:2
-
-            },
-            {
-                sub_activity_id:3,
-                max_score:2,
-                obtained_score:0.5
-
-            },
-            {
-                sub_activity_id:4,
-                max_score:2,
-                obtained_score:1
-
-            }
-        ],
-        score_status:"Finished"
-    },
-    {
-        id: 61010007,
-        prefix: "นาย",
-        firstname: "สมศักดิ์",
-        lastname: "ใฝ่รู้",
-        email: "61010007@kmitl.ac.th",
-        score: [
-            {
-                sub_activity_id:1,
-                max_score:2,
-                obtained_score:1.5
-
-            },
-            {
-                sub_activity_id:2,
-                max_score:2,
-                obtained_score:2
-
-            },
-            {
-                sub_activity_id:3,
-                max_score:2,
-                obtained_score:2
-
-            },
-            {
-                sub_activity_id:4,
-                max_score:2,
-                obtained_score:1.5
-
-            }
-        ],
-        score_status:"Finished"
-    },
-    {
-        id: 61010008,
-        prefix: "นาย",
-        firstname: "สมชาย",
-        lastname: "ใจดี",
-        email: "61010008@kmitl.ac.th",
-        score: [
-            {
-                sub_activity_id:1,
-                max_score:2,
-                obtained_score:null
-
-            },
-            {
-                sub_activity_id:2,
-                max_score:2,
-                obtained_score:null
-
-            },
-            {
-                sub_activity_id:3,
-                max_score:2,
-                obtained_score:null
-
-            },
-            {
-                sub_activity_id:4,
-                max_score:2,
-                obtained_score:null
-
-            }
-        ],
-        score_status:"Not Submitted"
-    },
-    {
-        id: 61010009,
-        prefix: "นาย",
-        firstname: "สมพงศ์",
-        lastname: "ชัยชนะ",
-        email: "61010009@kmitl.ac.th",
-        score: [
-            {
-                sub_activity_id:1,
-                max_score:2,
-                obtained_score:null
-
-            },
-            {
-                sub_activity_id:2,
-                max_score:2,
-                obtained_score:null
-
-            },
-            {
-                sub_activity_id:3,
-                max_score:2,
-                obtained_score:null
-
-            },
-            {
-                sub_activity_id:4,
-                max_score:2,
-                obtained_score:null
-
-            }
-        ],
-        score_status:"Not Submitted"
-    },
-    {
-        id: 61010010,
-        prefix: "นางสาว",
-        firstname: "สมสง่า",
-        lastname: "ราศี",
-        email: "61010010@kmitl.ac.th",
-        score: [
-            {
-                sub_activity_id:1,
-                max_score:2,
-                obtained_score:null
-
-            },
-            {
-                sub_activity_id:2,
-                max_score:2,
-                obtained_score:null
-
-            },
-            {
-                sub_activity_id:3,
-                max_score:2,
-                obtained_score:null
-
-            },
-            {
-                sub_activity_id:4,
-                max_score:2,
-                obtained_score:null
-
-            }
-        ],
-        score_status:"Not Submitted"
-    },
-    {
-        id: 61010011,
-        prefix: "นางสาว",
-        firstname: "สมหญิง",
-        lastname: "จริงใจ",
-        email: "61010011@kmitl.ac.th",
-        score: [
-            {
-                sub_activity_id:1,
-                max_score:2,
-                obtained_score:null
-
-            },
-            {
-                sub_activity_id:2,
-                max_score:2,
-                obtained_score:null
-
-            },
-            {
-                sub_activity_id:3,
-                max_score:2,
-                obtained_score:null
-
-            },
-            {
-                sub_activity_id:4,
-                max_score:2,
-                obtained_score:null
-
-            }
-        ],
-        score_status:"Not Submitted"
-    },
-];
-
-export const ActivityGrading = ({activity}) => {
-    const [students, setStudents] = useState([])
-    useEffect(() => {
-       setStudents(students_score)
-    }, [])
     return (
-        <GradingTable students={students} setStudents={setStudents} activity={activity}/>
+        <>
+            <div style={{ display: 'flex', justifyContent: "flex-end", gap: "0.5rem", paddingBottom: "1rem" }}>
+                <Tooltip title="กรอกครบแล้ว/ยังกรอกไม่ครบ/ยังไม่ได้กรอก" overlayStyle={{ maxWidth: '500px' }}>
+                    <div style={{ color: "white", display: "flex", textAlign: "center", fontSize: "18px" }}>
+                        <div style={{ backgroundColor: "#68A028", width: "32px", height: "32px", paddingTop: "0.2rem" }}>
+                            {students.filter((s) => s.score_status === "Finished").length}
+                        </div>
+                        <div style={{ backgroundColor: "#F7941D", width: "32px", height: "32px", paddingTop: "0.2rem" }}>
+                            {students.filter((s) => s.score_status === "Not Finished").length}
+                        </div>
+                        <div style={{ backgroundColor: "#C73535", width: "32px", height: "32px", paddingTop: "0.2rem" }}>
+                            {students.filter((s) => s.score_status === "Not Submitted").length}
+                        </div>
+                    </div>
+                </Tooltip>
+                <Button >Import</Button>
+            </div>
+            <Collapse accordion >
+                {students?.map((student, index) => {
+
+                    let calculatedScore = 0
+                    let totalMaxScore = 0
+                    student.score.forEach(element => {
+                        calculatedScore = calculatedScore + element.obtained_score || 0
+                        totalMaxScore = totalMaxScore + element.max_score || 0
+                    })
+                    return (
+                        <Panel
+                            header={
+                                <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                                    <Header level={5}>{student.id + "  " + student.prefix + " " + student.firstname + " " + student.lastname}</Header>
+                                    <div style={{ display: "flex", gap: "3rem", alignItems: "center", fontSize: "16px" }}>
+                                        <div>
+                                            <div>{calculatedScore} / {totalMaxScore}</div>
+                                        </div>
+                                        <div style={{ width: "120px", textAlign: "end" }}>
+                                            {
+                                                student.score_status === "Finished" &&
+                                                <div style={{ color: "#68A028" }}>
+                                                    {student.score_status}
+                                                </div>
+                                            }
+                                            {
+                                                student.score_status === "Not Finished" &&
+                                                <div style={{ color: "#F7941D" }}>
+                                                    {student.score_status}
+                                                </div>
+                                            }
+                                            {
+                                                student.score_status === "Not Submitted" &&
+                                                <div style={{ color: "#C73535" }}>
+                                                    {student.score_status}
+                                                </div>
+                                            }
+                                        </div>
+                                    </div>
+                                </div>
+
+                            }
+                            key={index}
+                        >
+                            <div style={{ height: "250px", overflow: "auto" }}>
+                                <div>
+                                    <Tabs defaultActiveKey="0" tabPosition={"left"} >
+                                        {subActivity?.map((subAct, i) => (
+                                            <Tabs.TabPane tab={subAct.title} key={i}>
+                                                <div className={styles.subActivityHeader}>
+                                                    <Header level={4}>{subAct.title + " " + subAct.detail}</Header>
+                                                    <Body level={2}>{student.score.filter((e) => e.sub_activity_id === subAct.id)[0].obtained_score} / {subAct.point}</Body>
+                                                </div>
+                                                <Rubric studentId={student.id} sub_activity_id={subAct.id}/>
+                                            </Tabs.TabPane>
+                                        ))}
+
+                                    </Tabs>
+                                </div>
+                            </div>
+                        </Panel>
+                    )
+                })}
+            </Collapse>
+        </>
     )
 }
+
+
+
+
+// <GradingTable students={students} setStudents={setStudents} activity={activity}/>

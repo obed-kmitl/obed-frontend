f033303eec131c8d7608672ad6dc28af99369bb0
Noppanat Panlutan <architon1997@gmail.com>
2022-02-14 21:14:21 +0700

    update production config

--- src/config.js
+++ src/config.js
@@ -5,6 +5,10 @@ const config = {
     apiUrl: process.env.REACT_APP_API_URL,
     googleClientId: process.env.REACT_APP_GOOGLE_CLIENT_ID || '1075818370617-olki93anh4mfpcasj4chcb9seci5leqj.apps.googleusercontent.com'
   },
+  production: {
+    apiUrl: process.env.REACT_APP_API_URL,
+    googleClientId: process.env.REACT_APP_GOOGLE_CLIENT_ID || '1075818370617-olki93anh4mfpcasj4chcb9seci5leqj.apps.googleusercontent.com'
+  },
 };
 
 export default config[nodeEnv];

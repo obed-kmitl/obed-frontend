4b85f96b8ca25f5d7b9da8746f570ba4c7c45e66
Nattarat Serirat <fangy2543@hotmail.com>
2022-02-20 14:41:15 +0700

    complete student group

--- src/components/ActivityGroup/ActivityGroup.jsx
+++ src/components/ActivityGroup/ActivityGroup.jsx
@@ -5,155 +5,132 @@ import {
     CloseOutlined, DeleteOutlined, EditOutlined
 } from '@ant-design/icons';
 import { Input, Popover, Modal, Form, Popconfirm } from 'antd';
-
-const groups = [
-    {
-        id: 1,
-        group_name: 'Group 1',
-        member: [61010001, 61010002, 61010003, 61010004]
-    },
-    {
-        id: 2,
-        group_name: 'Group 2',
-        member: [61010005, 61010006, 61010007,]
-    },
-    {
-        id: 3,
-        group_name: 'Group 3',
-        member: [61010008, 61010009, 61010010]
-    },
-    {
-        id: 4,
-        group_name: 'Group 4',
-        member: [61010011, 61019999, 61019998]
-    }
-]
-
-const students = [
-    {
-        id: 61010001,
-        prefix: "นางสาว",
-        firstname: "กมลชนก",
-        lastname: "ศรีไทย",
-        email: "61010001@kmitl.ac.th",
-    },
-    {
-        id: 61010002,
-        prefix: "นาย",
-        firstname: "ธนวัฒน์",
-        lastname: "สมมุติ",
-        email: "61010002@kmitl.ac.th",
-    },
-    {
-        id: 61010003,
-        prefix: "นาย",
-        firstname: "สมปอง",
-        lastname: "สุขสบาย",
-        email: "61010003@kmitl.ac.th",
-    },
-    {
-        id: 61010004,
-        prefix: "นาย",
-        firstname: "สมปราชญ์",
-        lastname: "สดใส",
-        email: "61010004@kmitl.ac.th",
-    },
-    {
-        id: 61010005,
-        prefix: "นาย",
-        firstname: "สมหมาย",
-        lastname: "สายไทย",
-        email: "61010005@kmitl.ac.th",
-    },
-    {
-        id: 61010006,
-        prefix: "นาย",
-        firstname: "สมหมาย",
-        lastname: "รักไทย",
-        email: "61010006@kmitl.ac.th",
-    },
-    {
-        id: 61010007,
-        prefix: "นาย",
-        firstname: "สมศักดิ์",
-        lastname: "ใฝ่รู้",
-        email: "61010007@kmitl.ac.th",
-    },
-    {
-        id: 61010008,
-        prefix: "นาย",
-        firstname: "สมชาย",
-        lastname: "ใจดี",
-        email: "61010008@kmitl.ac.th",
-    },
-    {
-        id: 61010009,
-        prefix: "นาย",
-        firstname: "สมพงศ์",
-        lastname: "ชัยชนะ",
-        email: "61010009@kmitl.ac.th",
-    },
-    {
-        id: 61010010,
-        prefix: "นางสาว",
-        firstname: "สมสง่า",
-        lastname: "ราศี",
-        email: "61010010@kmitl.ac.th",
-    },
-    {
-        id: 61010011,
-        prefix: "นางสาว",
-        firstname: "สมหญิง",
-        lastname: "จริงใจ",
-        email: "61010011@kmitl.ac.th",
-    },
-];
+import { useParams } from 'react-router-dom';
+import httpClient from '../../utils/httpClient';
 
 export const ActivityGroup = () => {
-    const [group, setGroup] = useState(groups)
+    const [group, setGroup] = useState()
+    const [students, setStudents] = useState()
     const [addingGroup, setAddingGroup] = useState(null)
     const [addGroupModal, setAddGroupModal] = useState(false)
     const [editingGroup, setEditingGroup] = useState(null)
     const [form] = Form.useForm()
 
-    function handleAddStudent(value) {
-        let added = [...group]
-        added.filter((e) => e.id === addingGroup)[0].member.push(value)
-        setGroup(added)
-        setAddingGroup(null)
+    let { activityId, sectionId } = useParams();
+
+    async function handleAddStudent(group_id, value) {
+        return await httpClient
+            .post(`/assessment/assignGroup/${group_id}`, {
+                student_id: value
+            })
+            .then(() => {
+                let added = [...group]
+                const student = students.filter((std) => std.student_id === value)[0]
+                added.filter((e) => e.group_id === addingGroup)[0].students.push(student)
+                setGroup(added)
+                setAddingGroup(null)
+            })
+            .catch((error) => {
+                console.log(error)
+            });
     }
 
-    function handleRemoveStudent(student, g_id) {
-        let removed = [...group]
-        removed.filter((e) => e.id === g_id)[0].member = removed.filter((e) => e.id === g_id)[0].member.filter((e) => e !== student);
-        setGroup(removed)
+    async function handleRemoveStudent(s_id, g_id) {
+        return await httpClient
+            .post(`/assessment/unassignGroup/${g_id}`, {
+                student_id: s_id
+            })
+            .then(() => {
+                let removed = [...group]
+                removed.filter((e) => e.group_id === g_id)[0].students = removed.filter((e) => e.group_id === g_id)[0].students.filter((e) => e.student_id !== s_id);
+                setGroup(removed)
+            })
+            .catch((error) => {
+                console.log(error)
+            });
     }
 
-    function handleAddGroup(value) {
-        setGroup([...group, {
-            id: Date.now(),//mock wait id from backend
-            group_name: value.group_name,
-            member: []
-        }])
-        form.resetFields()
-        setAddGroupModal(false)
+    async function handleAddGroup(value) {
+        return await httpClient
+            .post(`/assessment/createGroup/${activityId}`, {
+                title: value.title
+            })
+            .then((response) => {
+                setGroup([...group, {
+                    group_id: response.data.data.group_id,
+                    title: response.data.data.title,
+                    students: []
+                }])
+                form.resetFields()
+                setAddGroupModal(false)
+            })
+            .catch((error) => {
+                console.log(error)
+            });
     }
 
-    function handleDeleteGroup(id) {
-        setGroup(group.filter((g) => g.id !== id))
+    async function handleDeleteGroup(id) {
+        return await httpClient
+            .delete(`/assessment/removeGroup/${id}`)
+            .then(() => {
+                setGroup(group.filter((g) => g.group_id !== id))
+            })
+            .catch((error) => {
+                console.log(error)
+            });
     }
 
-    function handleEditGroup(value) {
-        console.log(value)
-        let edited = [...group]
-        edited.filter((g) => g.id === editingGroup)[0].group_name = value.group_name
-        setGroup(edited)
-        setEditingGroup(null)
-        form.resetFields()
+    async function handleEditGroup(value) {
+        return await httpClient
+            .put(`/assessment/updateGroup/${editingGroup}`, {
+                title: value.title
+            })
+            .then(() => {
+                let edited = [...group]
+                edited.filter((g) => g.group_id === editingGroup)[0].title = value.title
+                setGroup(edited)
+                setEditingGroup(null)
+                form.resetFields()
+            })
+            .catch((error) => {
+                console.log(error)
+            });
     }
+
+    async function fetchGroup() {
+        return await httpClient
+            .get(`/assessment/getAllGroupByActivity/${activityId}`)
+            .then((response) => {
+                setGroup(response.data.data.sort((a, b) => a.group_id - b.group_id))
+            })
+            .catch((error) => {
+                console.log(error)
+            });
+    }
+
+    async function fetchStudent() {
+        return await httpClient
+            .get(`/student/getAllBySection/${sectionId}`)
+            .then((response) => {
+                setStudents(response.data.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
     useEffect(() => {
-        form.setFieldsValue({ group_name: group.filter((g) => g.id === editingGroup)[0]?.group_name })
+        form.setFieldsValue({ title: group?.filter((g) => g.group_id === editingGroup)[0]?.title })
     }, [editingGroup])
+
+    useEffect(() => {
+        fetchGroup()
+        fetchStudent()
+    }, [])
+
+    useEffect(() => {
+        console.log(group);
+    }, [group])
 
     return (
         <div className={styles.group}>
@@ -163,17 +140,16 @@ export const ActivityGroup = () => {
                     <Button>Import</Button>
                     <Button onClick={() => setAddGroupModal(true)}>Add</Button>
                 </div>
-
             </div>
             <Collapse >
-                {group.map((g, index) =>
+                {group?.map((g, index) =>
                     <Panel
                         header={
                             <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-                                <Header level={3}>{g.group_name}</Header>
+                                <Header level={3}>{g.title}</Header>
                                 <div style={{ display: "flex", gap: "0.5rem" }}>
-                                    <EditOutlined style={{ color: "#009fc7" }} onClick={(e) => { e.stopPropagation(); setEditingGroup(g.id) }} />
-                                    <Popconfirm title="Delete this group?" onConfirm={() => handleDeleteGroup(g.id)}>
+                                    <EditOutlined style={{ color: "#009fc7" }} onClick={(e) => { e.stopPropagation(); setEditingGroup(g.group_id) }} />
+                                    <Popconfirm title="Delete this group?" onConfirm={() => handleDeleteGroup(g.group_id)}>
                                         <DeleteOutlined style={{ color: "#C73535" }} onClick={(e) => { e.stopPropagation(); }} />
                                     </Popconfirm>
                                 </div>
@@ -181,36 +157,34 @@ export const ActivityGroup = () => {
                         }
                         key={index}>
                         <div className={styles.container}>
-                            {g.member.map((student) =>
+                            {g.students.map((student) =>
                                 <Popover
                                     content={
-                                        students.filter((e) => e.id === student)[0]?.prefix + " " +
-                                        students.filter((e) => e.id === student)[0]?.firstname + " " +
-                                        students.filter((e) => e.id === student)[0]?.lastname
+                                        student.prefix + student.firstname + " " + student.lastname
                                     }
-                                    title={student}
+                                    title={student.student_number}
                                 >
                                     <div className={styles.student}>
-                                        {student}
-                                        <div onClick={() => handleRemoveStudent(student, g.id)}><CloseOutlined /></div>
+                                        {student.student_number}
+                                        <div onClick={() => handleRemoveStudent(student.student_id, g.group_id)}><CloseOutlined /></div>
                                     </div>
                                 </Popover>
                             )}
-                            {addingGroup === g.id ?
+                            {addingGroup === g.group_id ?
                                 <div className={styles.select}>
                                     <Select
                                         width={120}
-                                        onChange={(value) => handleAddStudent(value)}
+                                        onChange={(value) => handleAddStudent(g.group_id, value)}
                                         showSearch={true}
                                     >
                                         {students.map((std) =>
-                                            <Option value={std.id} disabled={g.member.includes(std.id)}>{std.id}</Option>
+                                            <Option value={std.student_id} disabled={g.students.map((e) => e.student_id).includes(std.student_id)}>{std.student_number}</Option>
                                         )}
                                     </Select>
                                     <CloseOutlined style={{ color: "#C73535" }} onClick={() => setAddingGroup(null)} />
                                 </div>
                                 :
-                                <div className={styles.addbtn} onClick={() => setAddingGroup(g.id)}>
+                                <div className={styles.addbtn} onClick={() => setAddingGroup(g.group_id)}>
                                     Add Student
                                 </div>
                             }
@@ -245,7 +219,7 @@ export const ActivityGroup = () => {
                 >
                     <Form.Item
                         label="Group Name"
-                        name="group_name"
+                        name="title"
                         rules={[
                             { required: true, message: "Please input Name" },
                         ]}
@@ -281,12 +255,12 @@ export const ActivityGroup = () => {
                 >
                     <Form.Item
                         label="Group Name"
-                        name="group_name"
+                        name="title"
                         rules={[
                             { required: true, message: "Please input Name" },
                         ]}
                     >
-                        <Input name='group_name' placeholder="Group Name" />
+                        <Input name='title' placeholder="Group Name" />
                     </Form.Item>
                 </Form>
             </Modal>

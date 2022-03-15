1120ff7397409381ea819a9fb98cf14080779def
Nattarat Serirat <fangy2543@hotmail.com>
2022-03-15 16:27:45 +0700

    add modify excel column width

--- src/utils/jsonToExcel.js
+++ src/utils/jsonToExcel.js
@@ -1,17 +1,19 @@
 import XLSX from 'xlsx'
 const EXCEL_EXTENSION = '.xlsx';
 
-export default function downloadAsExcel(json,title= "Sheet") {
+export default function downloadAsExcel(json, title = "Sheet", widthArray) {
     const workbook = XLSX.utils.book_new()
     const worksheet = XLSX.utils.json_to_sheet(json)
 
     workbook.Props = {
         Title: title,
-        Subject:  title,
+        Subject: title,
         Author: "OBED",
     };
-
+    if (widthArray) {
+        worksheet['!cols'] = widthArray
+    }
     XLSX.utils.book_append_sheet(workbook, worksheet, title);
 
-    XLSX.writeFile(workbook,title+EXCEL_EXTENSION)
+    XLSX.writeFile(workbook, title + EXCEL_EXTENSION)
 }
